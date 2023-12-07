597ba53b77a26b0919aad88c627570946f92fea6
xRedCrystalx <xredcrystaledx@gmail.com>
2023-12-07 22:59:37 +0100

    basic interaction classes

---
+++ src/crypto/transmitters/Client2API.py
@@ -0,0 +1,15 @@
+import sys, ccxt, typing
+sys.dont_write_bytecode = True
+
+
+class Sell:
+    def __init__(self):
+        ...
+
+class Buy:
+    def __init__(self):
+        ...
+
+class AccountInfo:
+    def __init__(self):
+        ...
