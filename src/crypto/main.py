1802e3d05c83ed0502ede34f2ce5985324dc95ed
xRedCrystalx <xredcrystaledx@gmail.com>
2023-12-08 23:11:02 +0100

    Sandbox mode runner & thread-safe variable sharing support

--- src/crypto/main.py
+++ src/crypto/main.py
@@ -1,9 +1,25 @@
-import ccxt, sys
+import ccxt, sys, asyncio
 sys.dont_write_bytecode = True
+import src.connector as con
 
 class CryptoMain:
     def __init__(self) -> None:
-        self.binance = ccxt.binance()
+        self.c: con.C | con.CNone = con.read_shared("colors")
+    
+    async def runner(self) -> None:
+        if con.read_shared("config")["general"]["sandbox"]:
+            print(f"{self.c.Gray}{"SANDBOX MODE IS ENABLED":-^45}{self.c.R}")
+            
+        
+        else:
+            self.binance = ccxt.binance({
+                "enableRateLimit" : True,
+                "apiKey" : con.read_shared("config")["binance"]["API-key"],
+                "secret" : con.read_shared("config")["binance"]["API-secret"]
+            })
+        
+        await con.terminate()
+
     
     def start(self) -> None:
-        ...
+        asyncio.run(self.runner())
