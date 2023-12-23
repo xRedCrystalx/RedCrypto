724ed0b865f3afd9034d783141b6341939a0aede
xRedCrystalx <xredcrystaledx@gmail.com>
2023-12-23 21:51:02 +0100

    Sandbox mode & requester loader

--- src/crypto/main.py
+++ src/crypto/main.py
@@ -1,24 +1,37 @@
 import ccxt, sys, asyncio
 sys.dont_write_bytecode = True
 import src.connector as con
+from src.crypto.transmitters.API2Client import PriceRequester
 
 class CryptoMain:
     def __init__(self) -> None:
         self.c: con.C | con.CNone = con.read_shared("colors")
+        self.config: dict = con.read_shared("config")
+        self.loop: asyncio.AbstractEventLoop = asyncio.get_running_loop()
+    
     
     async def runner(self) -> None:
+        # če je sandbox = True, zaženemo sandbox mode
         if con.read_shared("config")["general"]["sandbox"]:
             print(f"{self.c.Gray}{"SANDBOX MODE IS ENABLED":-^45}{self.c.R}")
             
         
+        # drugače začnemo normalno
         else:
             self.binance = ccxt.binance({
                 "enableRateLimit" : True,
-                "apiKey" : con.read_shared("config")["binance"]["API-key"],
-                "secret" : con.read_shared("config")["binance"]["API-secret"]
+                "apiKey" : self.config["binance"]["API-key"],
+                "secret" : self.config["binance"]["API-secret"]
             })
-        
-        await con.terminate()
+
+            # initializira requesterjain ga shrani
+            requester: PriceRequester = PriceRequester()
+            con.write_shared("price_requester", requester)
+            
+            # zažene requesterja v novem TASKu
+            self.loop.create_task(requester.start())
+
+        #await con.terminate()
 
     
     def start(self) -> None:
