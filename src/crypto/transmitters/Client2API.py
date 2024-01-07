7685c6b1fcd92bf1ab10c78b0db85890075d72b5
xRedCrystalx <xredcrystaledx@gmail.com>
2024-01-07 21:52:49 +0100

    Entire Buy/sell logic of Limit orders, with order handling

--- src/crypto/transmitters/Client2API.py
+++ src/crypto/transmitters/Client2API.py
@@ -1,14 +1,109 @@
-import sys, ccxt, typing
+import sys, ccxt, typing, asyncio
 sys.dont_write_bytecode = True
+import src.connector as con
+
+if True:
+    from ccxt.base.types import Order
+
+class Market:
+    def __init__(self) -> None:
+        self.config: dict[str, typing.Any] = con.read_shared("config")
+        self.exchange: ccxt.binance = con.read_shared("binance")
+        self.symbol: str = self.config["general"]["symbol"]
+        self.loop: asyncio.AbstractEventLoop = asyncio.get_running_loop()
+
+    async def _handle_open_order(self, order: Order, **kwargs) -> dict:
+        # shranmo kopijo originala
+        _order: Order = order
+
+        while order.status != "open":
+            await asyncio.sleep(2.5)
+            order = self.exchange.fetch_order(_order.id)
+        else:
+            if order.status == "rejected":
+                print(f"[{order.side.capitalize()}] > Order with id {order.id} was rejected by the exchange.")
+                return
+            else:
+                print(f"[{order.side.capitalize()}] > Transaction successful. {order.filled}/{order.amount} ID: {order.id}")
+
+                transaction_db: list[dict[str, typing.Any]] = con.read_shared("transaction_db")
+
+                if order.side == "buy":
+                    transaction_db.append({
+                        "id" : order.id,
+                        "quantity" : order.filled,
+                        "price" : kwargs["price"]
+                    })
+                else:
+                    transaction: str = kwargs["transaction"]
+                    transaction_db.remove(transaction)
+                
+                con.write_shared("transaction_db", transaction_db)
+    
+    async def sell(self, current_price: float, buy_transaction: dict[str, typing.Any] = None, quantity: float = None) -> bool:
+        if quantity:
+            order: Order = self.exchange.create_limit_sell_order(self.symbol, quantity, current_price-1)
+        elif buy_transaction:
+            order: Order = self.exchange.create_limit_sell_order(self.symbol, buy_transaction["quantity"], current_price-1)
+        
+        if order:
+            self.loop.create_task(self._handle_open_order(order, transaction=buy_transaction))
+            return True
+        
+        return False
+        
+
+    async def buy(self, quantity_in_precent: float, current_price: float) -> bool:
+        balance: float = await self.get_balance()
+        quantity: float = (balance / 100) * quantity_in_precent
+
+        if quantity:
+            order: Order = self.exchange.create_limit_buy_order(self.symbol, quantity, current_price+1)
+
+        if order:
+            self.loop.create_task(self._handle_open_order(order, price=current_price))
+            return True
+
+        return False
 
 
-async def sell() -> bool:
-    ...
+    async def get_balance(self) -> float:
+        return self.exchange.fetch_balance()["free"]["BTC"]
 
-async def buy() -> bool:
-    ...
 
-class AccountInfo:
-    def __init__(self):
-        ...
+#NOTE: PREGLED ZA PRICE, DA BO ZNAU DOLOČIT
+
+"""
+class Balance(TypedDict):
+    free: Num
+    used: Num
+    total: Num
+    debt: NotRequired[Num]
  
+
+
+ class Order(TypedDict):
+    info: Dict[str, Any]
+    id: Str
+    clientOrderId: Str
+    datetime: Str
+    timestamp: Int
+    lastTradeTimestamp: Int
+    lastUpdateTimestamp: Int
+    status: Str
+    symbol: Str
+    type: Str
+    timeInForce: Str
+    side: OrderSide
+    price: Num
+    average: Num
+    amount: Num
+    filled: Num
+    remaining: Num
+    stopPrice: Num
+    takeProfitPrice: Num
+    stopLossPrice: Num
+    cost: Num
+    trades: List[Trade]
+    fee: Fee
+ """
