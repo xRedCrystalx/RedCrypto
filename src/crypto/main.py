52d58d7d8f8b5ecf34692dc2d952fb1a71a3be1b
xRedCrystalx <xredcrystaledx@gmail.com>
2024-01-07 21:47:24 +0100

    Added comments, better sandbox handling

--- src/crypto/main.py
+++ src/crypto/main.py
@@ -1,27 +1,33 @@
-import ccxt, sys, asyncio
+import ccxt, sys, asyncio, typing
 sys.dont_write_bytecode = True
 import src.connector as con
 from src.crypto.transmitters.API2Client import PriceRequester
-from src.crypto.transmitters.sandbox import RequesterSimulator
+from src.crypto.transmitters.Client2API import Market
+from src.crypto.transmitters.sandbox import RequesterSimulator, MarketSimulator
+
 
 class CryptoMain:
     def __init__(self) -> None:
         self.c: con.C | con.CNone = con.read_shared("colors")
         self.config: dict = con.read_shared("config")
         self.loop: asyncio.AbstractEventLoop = asyncio.get_running_loop()
-    
+        self.crypto_task: asyncio.Task = None
     
     async def runner(self) -> None:
         # če je sandbox = True, zaženemo sandbox mode
-        if con.read_shared("config")["general"]["sandbox"]:
+        if con.read_shared("config")["general"]["sandbox"]["status"]:
             print(f"{self.c.Gray}{"SANDBOX MODE IS ENABLED":-^45}{self.c.R}")
             
-            # initializira requesterja in ga shrani
+            # initializira requesterja in market ter ga shrani
             requester: RequesterSimulator = RequesterSimulator()
             con.write_shared("price_requester", requester)
-            
+
+            market: MarketSimulator = MarketSimulator()
+            con.write_shared("market", market)
+
             # zažene requesterja v novem TASKu
-            self.loop.create_task(requester.start())
+            await requester.start()
+
         
         # drugače začnemo normalno
         else:
@@ -30,17 +36,25 @@ class CryptoMain:
                 "apiKey" : self.config["binance"]["API-key"],
                 "secret" : self.config["binance"]["API-secret"]
             })
+
             con.write_shared("binance", self.binance)
             
-            # initializira requesterja in ga shrani
+            # initializira requesterja in market ter ga shrani
             requester: PriceRequester = PriceRequester()
             con.write_shared("price_requester", requester)
+
+            market: Market = Market()
+            con.write_shared("market", market)
             
             # zažene requesterja v novem TASKu
-            self.loop.create_task(requester.start())
+            self.crypto_task: asyncio.Task = self.loop.create_task(requester.start())
 
         #await con.terminate()
 
     
     def start(self) -> None:
         asyncio.run(self.runner())
+
+    def stop(self) -> None:
+        if self.crypto_task:
+            self.crypto_task.cancel()
